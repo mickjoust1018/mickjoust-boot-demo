9ab40a434ea723565fa23fc587f5bd2bd96b90ef
mickjoust1018 <mickjoust@163.com>
2018-08-16 15:50:21 +0800

    new code

---
+++ boot-actuator/src/main/java/com/hjf/boot/demo/actuator/RunApp.java
@@ -0,0 +1,17 @@
+package com.hjf.boot.demo.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * name: com.hjf.boot.demo.actuator
+ * author: mickjoust
+ * date: 2018/5/17
+ **/
+@SpringBootApplication
+public class RunApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunApp.class,args);
+    }
+}
